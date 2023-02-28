f72f567a5e657c541073e205327c50107864e404
yuzupon1133 <yuzu18111@gmail.com>
2023-03-01 02:00:20 +0900

    update

--- getwiki.js
+++ getwiki.js
@@ -13,6 +13,41 @@ document.addEventListener("DOMContentLoaded", () => {
     if(!path.match(/\/$/)) {
       path += "/";
     }
+    let renderer = new marked.Renderer();
+    renderer.heading = function(text, level) {
+      if(text.split("\n")?.at(0) === "#info") {
+        let keys = text.split("\n").slice(1).map(value => {
+          return value.split(": ");
+        });
+        for(let key of keys) {
+          switch(key[0]) {
+            case "title":
+              $("title").innerText = key[1] + " | osu wiki";
+              break;
+            case "description":
+              $("meta[name=description]").setAttribute("content", key[1]);
+              break;
+            case "keywords":
+              $("meta[name=keywords]").setAttribute("content", key[1]);
+              break;
+            case "last_updated":
+              
+              break;
+          }
+        }
+        return "";
+      } else {
+        return `<h${level}>${text}</h${level}>`;
+      }
+    }
+    renderer.link = function(href, title, text) {
+      console.log(href, title, text);
+      if(!href.match(/^\//)) {
+        return `<a href="${href}" target="_blank" rel="noopener"${title ? ` title="${title}"` : ""}>${text ?? href}</a>`;
+      } else {
+        return `<a href="/osuwiki${href}"${title ? ` title="${title}"` : ""}>${text ?? href}</a>`;
+      }
+    }
     let start = Date.now();
     fetch(`/osuwiki/${path}index.md`)
     .then(res => {
@@ -21,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
       else console.log(res.status);
     })
     .then(res => {
-      $("main .wiki").innerHTML = marked.marked(res);
+      $("main .wiki").innerHTML = marked.marked(res, {renderer: renderer});
       $("main .header .time").innerText = (Date.now() - start) + "ms";
     });
   }
--- wiki/top_page/index.md
+++ wiki/top_page/index.md
@@ -1,1 +1,22 @@
-**こんにちは**
+---
+#info
+dir: /wiki/top_page/
+title: osu!の攻略サイト
+last_updated: 2023-02-28T22:43:00+09:00
+---
+
+## osu wikiへようこそ！
+このwikiは無料のリズムゲーム「osu!」の非公式wikiです。
+
+公式サイト: https://osu.ppy.sh/ \
+公式wiki: https://osu.ppy.sh/wiki/ja/Main_Page/
+
+## もくじ
+- [osu!のはじめ方](/wiki/how_to/start_osu/)
+- [osu!のルール](/wiki/rules/)
+- [譜面の入れ方](/wiki/how_to/import_beatmap/)
+- [プレイ方法](/wiki/how_to/play_gamemodes/)
+- [osu!の用語一覧](/wiki/terms/)
+- [全メダル取得方法](/wiki/how_to/get_medals/)
+- [osu!のチーム一覧](/wiki/teams/)
+- [システムファイル一覧](/wiki/system_files/)
