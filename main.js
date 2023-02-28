c3e4aaa5da4b3a8f44f11d8540152c709ada8235
Yukinyaaa <65931718+Yukinyaaa@users.noreply.github.com>
2023-02-28 16:17:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   let pagetop = document.querySelector(".page-top");
-    document.addEventListener("scroll", () => {
-      if(window.pageYOffset <= 70) {
-        pagetop.setAttribute("hidden", "");
-      } else {
-        pagetop.removeAttribute("hidden");
-      }
+  let top = document.querySelector("nav .top");
+  document.addEventListener("scroll", () => {
+    if(window.pageYOffset <= 70) {
+      pagetop.setAttribute("hidden", "");
+      top.setAttribute("hidden", "");
+    } else {
+      pagetop.removeAttribute("hidden");
+      top.removeAttribute("hidden");
+    }
+  });
+  document.dispatchEvent(new Event("scroll"));
+  pagetop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
-    pagetop.addEventListener("click", () => {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    });
+  });
+  
+  let nav = document.querySelector("nav");
+  let hamburger = document.querySelector("nav .hamburger");
+  let menu = document.querySelector("nav .menu");
+  hamburger.addEventListener("click", () => {
+    pagetop.classList.toggle("menu-show");
+    nav.classList.toggle("menu-show");
+    menu.classList.toggle("menu-show");
+  });
 });
