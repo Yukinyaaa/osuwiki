0110d5bcff5b453fa91c344fcf56f4996e0d3fe1
yuzupon1133 <yuzu18111@gmail.com>
2023-03-02 16:01:42 +0900

    fix underline bug

--- js/getwiki.js
+++ js/getwiki.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }, 200);
   function main() {
+    let svgs = {
+      home: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M543.8 287.6c17 0 32-14 32-32.1c1-9-3-17-11-24L512 185V64c0-17.7-14.3-32-32-32H448c-17.7 0-32 14.3-32 32v36.7L309.5 7c-6-5-14-7-21-7s-15 1-22 8L10 231.5c-7 7-10 15-10 24c0 18 14 32.1 32 32.1h32v69.7c-.1 .9-.1 1.8-.1 2.8V472c0 22.1 17.9 40 40 40h16c1.2 0 2.4-.1 3.6-.2c1.5 .1 3 .2 4.5 .2H160h24c22.1 0 40-17.9 40-40V448 384c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v64 24c0 22.1 17.9 40 40 40h24 32.5c1.4 0 2.8 0 4.2-.1c1.1 .1 2.2 .1 3.3 .1h16c22.1 0 40-17.9 40-40V455.8c.3-2.6 .5-5.3 .5-8.1l-.7-160.2h32z"/></svg>',
+      folder: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z"/></svg>',
+    }
     let path = location.pathname.match(/^\/osuwiki\/(.*)/)[1];
     let dir = path.match(/^(wiki\/|debug\/).*/);
     if(path === "" || dir) {
@@ -14,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
         path = "wiki/" + location.search.slice(2);
       }
       if(!path.match(/\/$/)) path += "/";
+      $("main .header .dir");
       let renderer = new marked.Renderer();
       renderer.heading = function(text, level) {
         if(text.split("\n")[0] === "#info") {
@@ -81,7 +86,6 @@ document.addEventListener("DOMContentLoaded", () => {
         $("main .header .parse-time").innerText = (parse_end - req_end) + "ms";
         let strong = $("main .wiki strong");
         document.addEventListener("scroll", () => {
-          console.clear();
           for(let i = 0; i < strong.length; i++) {
             let pos = strong[i].getBoundingClientRect().top + window.scrollY;
             if(window.scrollY > pos - window.innerHeight) {
