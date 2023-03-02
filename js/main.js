643c1b45eaa2f6e82c4eed17f3ff680affcd85c2
yuzupon1133 <yuzu18111@gmail.com>
2023-03-02 22:42:41 +0900

    dark mode support

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ function $(name) {
   let nodes = document.querySelectorAll(name);
   return nodes.length == 1 ? nodes[0] : nodes;
 }
+if(localStorage.getItem("darkmode") == "true") {
+  let id = setInterval(() => {
+    if(document.body) {
+      document.body.classList.add("dark");
+      clearInterval(id);
+    }
+  }, 10);
+}
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("loaded");
   document.addEventListener("scroll", () => {
@@ -26,5 +34,15 @@ document.addEventListener("DOMContentLoaded", () => {
   
   $("main .header .sum-time").addEventListener("click", () => {
     $("main .header .detail").classList.toggle("detail-show");
+  });
+  
+  $("nav .dark-switcher").addEventListener("click", () => {
+    if(localStorage.getItem("darkmode") != "false") {
+      document.body.classList.remove("dark");
+      localStorage.setItem("darkmode", "false");
+    } else {
+      document.body.classList.add("dark");
+      localStorage.setItem("darkmode", "true");
+    }
   });
 });
